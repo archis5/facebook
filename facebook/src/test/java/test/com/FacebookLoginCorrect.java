69cf8e2ce14c2207778634870d49aab929709507
archis5 <102880864+archis5@users.noreply.github.com>
2022-04-22 22:23:37 -0500

    myfacebook

---
+++ facebook/src/test/java/test/com/FacebookLoginCorrect.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test.com;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Owner
+ */
+public class FacebookLoginCorrect {
+    
+    private WebDriver driver;
+    private String baseUrl;
+    
+    public FacebookLoginCorrect() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+     @Test
+  public void testLoginCorrect() throws Exception {
+      System.setProperty("webdriver.chrome.driver", "c:\\data\\chromedriver.exe");
+    driver = new ChromeDriver();
+    driver.get("https://www.facebook.com/");
+    driver.findElement(By.id("email")).click();
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("archis5@ukr.net");
+    driver.findElement(By.id("pass")).click();
+    driver.findElement(By.id("pass")).clear();
+    driver.findElement(By.id("pass")).sendKeys("starik85");
+    driver.findElement(By.id("login")).click();
+  }
+
+}
